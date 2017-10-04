5d054950fa9625bc221347b67a618b47732fddac
Sanjay Zala <sanjaysinhz@officebrain.com>
2017-10-04 11:10:14 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+  console.log('New User Connected');
+  
+  socket.on('disconnect', function(){
+    console.log('User Disconnected');
+  });
+  
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+  });
+
+})
+
+http.listen(2200, function(){
+  console.log('listening on *:2200');
+});
